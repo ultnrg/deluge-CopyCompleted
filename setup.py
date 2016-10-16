3645e11f15397285c30d45930a711e7a4c952679
ultnrg <nicolas@CRESTA.local>
2016-10-16 12:52:30 +1100

    Fixed setup version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ from setuptools import setup
 __plugin_name__ = "CopyCompleted"
 __author__ = "Sam Lai"
 __author_email__ = "sam@edgylogic.com"
-__version__ = "0.6"
+__version__ = "0.7"
 __url__ = "http://deluge-torrent.org"
 __license__ = "GPLv3"
 __description__ = "Copies completed torrents to another directory."
